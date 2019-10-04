405ad39bd89eae6739a6ac56ff330aa0cdd29b48
Steven Thwaites <steven.thwaites@docker.com>
2019-10-04 15:14:36 +0100

    Refresh cat gif URLs

--- app.py
+++ app.py
@@ -7,17 +7,14 @@ app = Flask(__name__)
 # list of cat images
 images = [
     "http://i.giphy.com/Nm8ZPAGOwZUQM.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr01/15/9/anigif_enhanced-buzz-31540-1381844535-8.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26390-1381844163-18.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/10/anigif_enhanced-buzz-1376-1381846217-0.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/9/anigif_enhanced-buzz-3391-1381844336-26.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/10/anigif_enhanced-buzz-29111-1381845968-0.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/9/anigif_enhanced-buzz-3409-1381844582-13.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr02/15/9/anigif_enhanced-buzz-19667-1381844937-10.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26358-1381845043-13.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/9/anigif_enhanced-buzz-18774-1381844645-6.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/9/anigif_enhanced-buzz-25158-1381844793-0.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/10/anigif_enhanced-buzz-11980-1381846269-1.gif"
+    "https://i.giphy.com/JIX9t2j0ZTN9S.gif",
+    "https://i.giphy.com/8u9PS5l5znIbe.gif",
+    "https://i.giphy.com/82CItLnbSh8hzsXK3H.gif",
+    "https://i.giphy.com/jFxlUfJkHdz7W.gif",
+    "https://i.giphy.com/IUjNSNM9VEUH6.gif",
+    "https://i.giphy.com/O5pd2oPjjcGFW.gif",
+    "https://i.giphy.com/l0IydrYaxclPhGG76.gif",
+    "https://i.giphy.com/WQ9ghbypX4mly.gif"
 ]
 
 @app.route('/')
